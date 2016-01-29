b48687671ea6ddaab6f5d81ce5f97377277c1e68
spencerahill <spencerahill@gmail.com>
2016-01-29 01:05:55 -0500

    TEST create bwd_diff test suite

--- infinite_diff/test/test_finite_diff.py
+++ infinite_diff/test/test_finite_diff.py
@@ -21,42 +21,52 @@ class FiniteDiffTestCase(unittest.TestCase):
                             for n in range(self._array_len)]
         self.arange = xr.DataArray(np.arange(self._array_len), dims=[self.dim])
         self.arange_trunc = [self.arange.isel(**{self.dim: slice(n, None)})
-                            for n in range(self._array_len)]
+                             for n in range(self._array_len)]
 
     def tearDown(self):
         pass
 
 
-# method = FiniteDiff.fwd_diff
-method = FiniteDiff.bwd_diff
+class FwdDiffTestCase(FiniteDiffTestCase):
+    def setUp(self):
+        super(FwdDiffTestCase, self).setUp()
+        self.method = FiniteDiff.fwd_diff
 
 
-class TestFiniteDiff(FiniteDiffTestCase):
+class TestFwdDiff(FwdDiffTestCase):
     def test_bad_spacing(self):
-        self.assertRaises(ValueError, method, self.ones, self.dim,
+        self.assertRaises(ValueError, self.method, self.ones, self.dim,
                           **{'spacing': 0})
 
     def test_bad_array_len(self):
         for n in range(len(self.ones[self.dim])):
-            self.assertRaises(ValueError, method,
+            self.assertRaises(ValueError, self.method,
                               self.ones_trunc[n], self.dim,
                               **{'spacing': self._array_len - n})
 
     def test_zero_slope(self):
         for n, zeros in enumerate(self.zeros_trunc[1:]):
             # Array len gets progressively smaller.
-            ans = method(self.ones_trunc[n],
-                         self.dim, spacing=1)
+            ans = self.method(self.ones_trunc[n], self.dim, spacing=1)
             np.testing.assert_array_equal(ans, zeros)
             # Spacing of differencing gets progressively larger.
-            ans = method(self.ones, self.dim, spacing=n+1)
+            ans = self.method(self.ones, self.dim, spacing=n+1)
             np.testing.assert_array_equal(ans, zeros)
 
     def test_constant_slope(self):
         for n, arange in enumerate(self.arange_trunc[:-1]):
-            ans = method(arange, self.dim, spacing=1)
+            # Array len gets progressively smaller.
+            ans = self.method(arange, self.dim, spacing=1)
             np.testing.assert_array_equal(ans, self.ones_trunc[n+1])
-
+            # Spacing of differencing gets progressively larger.
+            ans = self.method(self.arange, self.dim, spacing=n+1)
+            np.testing.assert_array_equal(ans, (n+1)*self.ones_trunc[n+1])
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
+
+
+class TestBwdDiff(TestFwdDiff):
+    def setUp(self):
+        super(TestBwdDiff, self).setUp()
+        self.method = FiniteDiff.bwd_diff
