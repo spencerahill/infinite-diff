5473508061f28b12de834e30e3f5e0bfdbb0c760
Spencer Hill <shill@atmos.ucla.edu>
2017-04-06 17:21:07 -0700

    Travis CI and Appveyor setup (#9)
    * Add needed files for Travis CI and AppVeyor 2.7 & 3.4-3.6 builds
    * Add `install_requires` and `test_require` fields to setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,15 @@ setuptools.setup(
     version="0.0",
     packages=setuptools.find_packages(),
     author="Spencer A. Hill",
-    author_email="spencerahill@gmail.com",
+    author_email="shill@atmos.ucla.edu",
     description="Xarray-based finite differencing of gridded geophysical data",
+    install_requires=['numpy >= 1.7',
+                      'toolz >= 0.7.2',
+                      'cloudpickle >= 0.2.1',
+                      'xarray >= 0.9.1'],
+    tests_require=['pytest >= 2.7.1'],
     license="Apache",
-    keywords="climate science",
+    keywords="climate science, xarray, finite differencing",
     url="https://github.com/spencerahill/infinite-diff",
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -18,6 +23,7 @@ setuptools.setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Atmospheric Science'
     ]
 )
